3114fbbf626e92b88efaae59a88398469cc3842b
MD. Akil Mahmud <mdakilmahmud.ak@gmail.com>
2024-12-28 14:50:55 +0600

    Delete Problem 17.py

--- Problem 17.py
+++
@@ -1,9 +0,0 @@
-# hogwarts=[“Harry”,”Ron”,”Hermione”, “Dobby”, “Bellatrix”, “Voldemort”, “Ginny”, “Luna”, “Snape”] Write a program to filter out names from the list that have more than 5 letters.
-# Sample Input:
-# Sample Output:Hermione | Bellatrix| Voldemort
-
-# Solution:
-hogwarts = ['Harry', 'Ron', 'Hermione', 'Dobby', 'Bellatrix', 'Voldemort', 'Ginny', 'Luna', 'Snape']
-for name in hogwarts:
-      if len(name)>5:
-            print(name)
